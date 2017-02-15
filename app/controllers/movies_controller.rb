63729eaac969a12f63b41cece87f9b6d184b54d4
Alex Markowitz <admarko@gmail.com>
2017-02-15 00:18:32 -0600

    100%

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,6 @@ class MoviesController < ApplicationController
   end
 
 
-
   #CREATE
   def new_form
     render("movies/new_form.html.erb")
@@ -23,13 +22,11 @@ class MoviesController < ApplicationController
     @movie.duration = params[:the_duration]
     @movie.description = params[:the_description]
     @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.director_id = params[:director_id]
     @movie.save
 
-    render("movies/show.html.erb")
+    redirect_to("http://localhost:3000/movies")
   end
-
-
 
 
   #UPDATE
